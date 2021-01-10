57ff4c5a86f926820af20ffdbb2313a42c30b057
unknown <ghinea.alexandru.george@gmail.com>
2021-01-11 00:57:09 +0200

    exercise 3

--- Chapter8Solutions.R
+++ Chapter8Solutions.R
@@ -25,4 +25,75 @@ ggtsdisplay(ibmclose)
 
 ## Exercise 3
 
+###For the following series, find an appropriate Box-Cox transformation and order of differencing in order to obtain stationary data.
 
+#a. usnetelec
+plot(usnetelec)
+#Avand in vedere forma seriei, putem observa o crestere usor liniara
+plot(diff(usnetelec))
+Acf(diff(usnetelec), lag.max = 100)
+
+Pacf(diff(usnetelec), lag.max = 100)
+
+#Functiile ACF si PACF arata ca diferentele dintre seriile de date se comporta aproape ca un white
+
+#b. usgdp
+
+plot(usgdp)
+Acf(usgdp, lag.max = 100)
+
+Pacf(usgdp, lag.max = 100)
+
+#Exista o valoare care este aproape 1, deci trebuie diferentiat (procesul nefiind non-stationar). Nu putem aplica modelul ARIMA fara diferentiere
+
+lambda = BoxCox.lambda(usgdp)
+plot(diff(BoxCox(usgdp,lambda)))
+
+#Vom alege o valoare lambda pentru a diferentia cu o anumita putere
+
+Acf(diff(BoxCox(usgdp, lambda)), lag.max = 100)
+Pacf(diff(BoxCox(usgdp, lambda)), lag.max = 100)
+
+#Dupa diferentierea realizata cu valoarea lambda putem observa ca seria de timp este stationara
+
+#c. mcopper
+
+plot(mcopper)
+
+Acf(mcopper, lag.max = 100)
+
+Pacf(mcopper, lag.max = 100)
+
+lambda = BoxCox.lambda(mcopper)
+plot(diff(BoxCox(mcopper, lambda)))
+
+Acf(diff(BoxCox(mcopper, lambda)), lag.max = 100)
+### ACF scade rapid cu cateva perturbatii pentru valorile mari de lag
+Pacf(diff(BoxCox(mcopper, lambda)), lag.max = 100)
+
+### ACF si PACF sunt stationare
+
+#d. enplanements
+
+plot(enplanements)
+plot(log(enplanements))
+
+Acf(log(enplanements), lag.max = 100)
+
+### Datele trebuie diferentiate pentru a aplica modelul ARIMA. Cand aplicam log pe ACF, datele nu converg catre valori critice
+
+Pacf(log(enplanements), lag.max = 100)
+
+### Seria de timp trebuie diferentiata pentru a aplica modelul Arima. Datele sunt sezoniere si trebuie diferentiate cu un lag egal cu numarul de sezoane
+plot(diff(log(enplanements), lag = 12))
+acf(diff(log(enplanements), lag = 12), lag.max = 100)
+pacf(diff(log(enplanements), lag = 12), lag.max = 100)
+
+### Trebuie diferentiate de doua ori
+
+plot(diff(diff(log(enplanements), lag = 12)))
+
+acf(diff(diff(log(enplanements), lag = 12)), lag.max = 100)
+pacf(diff(diff(log(enplanements), lag = 12)), lag.max = 100)
+
+#e. visitors
